0d27efd1f1b3c4e37959e6f8f303dbabb68b1ae4
Jim McQuarrie <coderjams@gmail.com>
2024-12-02 02:48:54 -0500

    test a utils file jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { pluck, range } from './shared/utils/utils';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    console.log('app', range(1, 5)); // [1, 2, 3, 4]
+    console.log(pluck([{ name: 'John' }, { name: 'Doe' }], 'name')); // ['
+  }
+}
